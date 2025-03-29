d476876640eb4ba9671b8d69469d30be8105289e
Vanshika Dhiman <vanshika9dhiman@gmail.com>
2025-03-29 10:52:21 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,8 +2,8 @@ int fibonacciSeries(int n){
     int first=0;
     int second=1;
     int arr[n];
-    int arr[0]=first;
-    int arr[1]=second;
+    arr[0]=first;
+     arr[1]=second;
     for(int i=2;i<n;i++){
         int sum=first+second;
         arr[i]=sum;
