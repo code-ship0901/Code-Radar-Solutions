3668a42dd3344650fb541835c43b5cb27342f57a
Vanshika Dhiman <vanshika9dhiman@gmail.com>
2025-03-30 12:09:05 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -8,7 +8,7 @@ int main(){
     }
     for (int i=0;i<n;i++){
         for (int j=0;j<n-i-1;j++){
-            if(arr[j]>ar[j+1]){
+            if(arr[j]>arr[j+1]){
                 arr[j+1]=arr[j];
             }
         }
